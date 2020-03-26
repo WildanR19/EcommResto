4e427abb67e6bb07101cb2ac9ad900e762cb8fc9
WildanR19 <hyakuya7856@gmail.com>
2020-03-26 21:30:55 +0700

    removed index.php

--- application/modules/admin/views/_partials/js.php
+++ application/modules/admin/views/_partials/js.php
@@ -4,14 +4,14 @@
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="<?php echo base_url('../assets/vendor/jquery-easing/jquery.easing.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/vendor/jquery-easing/jquery.easing.min.js'); ?>"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="<?php echo base_url('../assets/js/sb-admin-2.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/js/sb-admin-2.min.js'); ?>"></script>
 
   <!-- Page level plugins -->
-  <script src="<?php echo base_url('../assets/vendor/chart.js/Chart.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/vendor/chart.js/Chart.min.js'); ?>"></script>
 
   <!-- Page level custom scripts -->
-  <script src="<?php echo base_url('../assets/js/demo/chart-area-demo.js'); ?>"></script>
-  <script src="<?php echo base_url('../assets/js/demo/chart-pie-demo.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/js/demo/chart-area-demo.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/js/demo/chart-pie-demo.js'); ?>"></script>
--- application/modules/login/views/v_register.php
+++ application/modules/login/views/v_register.php
@@ -12,14 +12,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
   
   <!-- Font Awesome -->
-	<link rel="stylesheet" href="<?php echo base_url('../assets/fontawesome-free/css/all.min.css') ?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/fontawesome-free/css/all.min.css') ?>">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- icheck bootstrap -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <!-- Theme style -->
-	<link rel="stylesheet" href="<?php echo base_url('../assets/css/adminlte.min.css'); ?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/adminlte.min.css'); ?>">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
@@ -116,6 +116,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!-- Bootstrap 4 -->
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <!-- AdminLTE App -->
-<script src="<?php echo base_url('../assets/js/adminlte.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/adminlte.min.js');?>"></script>
 </body>
 </html>
