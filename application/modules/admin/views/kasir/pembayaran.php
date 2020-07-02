dd48f8aa9ea02378f7b69bbb9148cf311af5b681
WildanR19 <hyakuya7856@gmail.com>
2020-07-02 17:09:01 +0700

    fix tampilan2

--- application/modules/admin/views/kasir/pembayaran.php
+++ application/modules/admin/views/kasir/pembayaran.php
@@ -31,7 +31,28 @@ $this->load->view("admin/_partials/head.php") ?>
                     </div>
                     <div class="col-4">
                         <div class="input-group justify-content-end">
-                            <input type="text" class="form-control bg-white border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" id="keywords" onkeyup="searchFilter();" />
+                            <div class="d-none d-sm-inline-block">
+                                <input type="text" class="form-control bg-white border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" id="keywords" onkeyup="searchFilter();" />
+                            </div>
+                            
+                            <li class="nav-item dropdown no-arrow d-sm-none">
+                                <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="fas fa-search fa-fw"></i>
+                                </a>
+                                <!-- Dropdown - Messages -->
+                                <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                                    <form class="form-inline mr-auto w-100 navbar-search">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" id="keywords" onkeyup="searchFilter();">
+                                        <div class="input-group-append">
+                                        <button class="btn btn-primary" type="button">
+                                            <i class="fas fa-search fa-sm"></i>
+                                        </button>
+                                        </div>
+                                    </div>
+                                    </form>
+                                </div>
+                            </li>
                             <select id="sortBy" onchange="searchFilter();" class="custom-select">
                                 <option value="">Sort by ID</option>
                                 <option value="asc">Sort by Name</option>
